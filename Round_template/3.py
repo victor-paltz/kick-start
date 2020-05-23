edbc4e7675c08245a6ded35f0b78a5c36844ed25
Victor Paltz <paltz.victor@free.fr>
2020-05-24 00:51:50 +0200

    template for fast code

---
+++ Round_template/3.py
@@ -0,0 +1,21 @@
+
+import sys
+
+
+def readstr(): return sys.stdin.readline().strip()
+
+
+def readint(): return int(readstr())
+
+
+def readints(): return [int(x) for x in readstr().split()]
+
+
+def readinttab(n): return [readints() for _ in range(n)]
+
+
+T = readint()
+
+for test in range(1, T+1):
+
+    print("Case #{}: {}".format(test, 0))
